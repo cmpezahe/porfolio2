0e78254db09cc6830494a02bd798a4185ce12864
cmpezahe <cmpezahe@outlook.com>
2023-07-10 00:12:03 +0100

    'Second Commit'

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const sr = ScrollReveal({
     distance: '35px',
     duration: 2400,
-    reset: true
+    reset: false
 });
 
 sr.reveal('.logo', {delay:210, origin: 'left'});
@@ -9,8 +9,14 @@ sr.reveal('.navlist', {delay:310, origin: 'top'});
 sr.reveal('.top-btnn', {delay:410, origin: 'right'});
 
 
-sr.reveal('.main-content h4', {delay:510, origin: 'top'});
-sr.reveal('.main-content h1', {delay:610, origin: 'left'});
-sr.reveal('.main-content p', {delay:710, origin: 'right'});
-sr.reveal('.social', {delay:810, origin: 'left'});
-sr.reveal('.main-btnn', {delay:910, origin: 'top'});
+const srs = ScrollReveal({
+    distance: '35px',
+    duration: 2400,
+    reset: true
+});
+
+srs.reveal('.main-content h4', {delay:210, origin: 'top'});
+srs.reveal('.main-content h1', {delay:310, origin: 'left'});
+srs.reveal('.main-content p', {delay:410, origin: 'right'});
+srs.reveal('.social', {delay:510, origin: 'left'});
+srs.reveal('.main-btnn', {delay:610, origin: 'top'});

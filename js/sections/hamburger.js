b607f7ecec723de1c35f5e5919a8732b1b5d7c71
cmpezahe <cmpezahe@outlook.com>
2023-07-13 23:38:47 +0100

    'Layout Changes'

--- js/sections/hamburger.js
+++ js/sections/hamburger.js
@@ -1,10 +1,24 @@
 var burger = document.querySelector(".hamburger");
 var navUl = document.querySelector(".navlist");
 
+// Function to close the menu
+function closeMenu() {
+  burger.classList.remove("active");
+  navUl.classList.remove("active");
+}
+
 burger.addEventListener("click", function () {
   burger.classList.toggle("active");
   navUl.classList.toggle("active");
 });
+
+// Add event listener to anchor tags
+var anchorTags = document.querySelectorAll(".navlist a");
+for (var i = 0; i < anchorTags.length; i++) {
+  anchorTags[i].addEventListener("click", function () {
+    closeMenu();
+  });
+}
 
 document.addEventListener("click", function (event) {
   // If the clicked element is not the navbar, a link, or the hamburger icon
@@ -14,7 +28,6 @@ document.addEventListener("click", function (event) {
     !burger.contains(event.target)
   ) {
     // Remove the "active" class from the navbar to close it
-    navUl.classList.remove("active");
-    burger.classList.remove("active");
+    closeMenu();
   }
 });
